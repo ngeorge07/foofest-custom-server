9dd3138be2d9ed521ad277b0d53e27ed24fbbe34
George <geor2335@stud.kea.dk>
2022-05-31 06:41:18 +0200

    added cancelled

--- src/Information.js
+++ src/Information.js
@@ -58,6 +58,7 @@ class Information {
       const slot = rndBetweenEven(0, 10);
 
       if (!this.slots[scene][day][slot].cancelled) {
+        this.slots[scene][day][slot].cancelled = true;
         observer.publish(events.CANCELLATION, this.slots[scene][day][slot]); //TODO args, which event
         this.slots[scene][day][slot].cancelled = true;
       }
